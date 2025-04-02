eab5692e3acea6940fee265bab4595de66e2562c
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-04-02 19:20:20 -0400

    feat: Add Spinner.jsx, improve MovieCard.jsx, integrate Appwrite DB

---
+++ src/appwrite.jsx
@@ -0,0 +1,70 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID; // Fixed typo
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID; // Fixed typo
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    try {
+        const result = await database.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID,
+            [Query.equal('searchTerm', searchTerm)]
+        );
+
+        if (result.documents.length > 0) {
+            const doc = result.documents[0];
+            const updateResponse = await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                doc.$id,
+                {
+                    count: doc.count + 1,
+                    movie_id: movie.id,
+                }
+            );
+            console.log("Appwrite Update Success:", updateResponse);
+            return updateResponse;
+        } else {
+            const createResponse = await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    searchTerm,
+                    count: 1,
+                    movie_id: movie.id,
+                    poster_url: movie.poster_path
+                        ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+                        : null,
+
+                }
+            );
+            console.log("Appwrite Create Success:", createResponse);
+            return createResponse;
+        }
+    } catch (error) {
+        console.error("Appwrite Error:", error);
+        throw error;
+    }
+};
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ]);
+
+        return result.documents
+    } catch (error) {
+
+    }
+
+}
